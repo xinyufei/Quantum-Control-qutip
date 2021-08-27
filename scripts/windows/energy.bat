bb1e932423eaf6bb0da57fe9a50bab63e5338fc9
Xinyu <xyfei27@gmail.com>
2021-08-26 22:38:13 -0400

    update sur rounding results

--- scripts/windows/energy.bat
+++ scripts/windows/energy.bat
@@ -4,29 +4,29 @@ python energy.py --n=2 --num_edges=1 --evo_time=2 --n_ts=40^
     --initial_control="../control/Continuous/Energy2_evotime2.0_n_ts40_ptypeCONSTANT_offset0.5.csv"^
     --type=SUR
 
-python energy.py --n=2 --num_edges=1 --evo_time=2 --n_ts=40^
+#python energy.py --n=2 --num_edges=1 --evo_time=2 --n_ts=40^
     --initial_control="../control/Continuous/Energy2_evotime2.0_n_ts40_ptypeCONSTANT_offset0.5.csv"^
     --type=minup --min_up=10
-python energy.py --n=2 --num_edges=1 --evo_time=2 --n_ts=40^
+#python energy.py --n=2 --num_edges=1 --evo_time=2 --n_ts=40^
     --initial_control="../control/Continuous/Energy2_evotime2.0_n_ts40_ptypeCONSTANT_offset0.5.csv"^
     --type=maxswitch --max_switch=5
 python energy.py --n=2 --num_edges=1 --evo_time=2 --n_ts=40^
     --initial_control="../control/ADMM/EnergyADMM2_evotime2.0_n_ts40_ptypeWARM_offset0.5_penalty0.01_ADMM_10.0_iter100.csv"^
     --type=SUR
-python energy.py --n=2 --num_edges=1 --evo_time=2 --n_ts=40^
+#python energy.py --n=2 --num_edges=1 --evo_time=2 --n_ts=40^
     --initial_control="../control/ADMM/EnergyADMM2_evotime2.0_n_ts40_ptypeWARM_offset0.5_penalty0.01_ADMM_10.0_iter100.csv"^
     --type=minup --min_up=10
-python energy.py --n=2 --num_edges=1 --evo_time=2 --n_ts=40^
+#python energy.py --n=2 --num_edges=1 --evo_time=2 --n_ts=40^
     --initial_control="../control/ADMM/EnergyADMM2_evotime2.0_n_ts40_ptypeWARM_offset0.5_penalty0.0001_ADMM_10.0_iter100.csv"^
     --type=maxswitch --max_switch=5
 
 python energy.py --n=2 --num_edges=1 --evo_time=2 --n_ts=40^
     --initial_control="../control/ADMM/EnergyADMM2_evotime2.0_n_ts40_ptypeWARM_offset0.5_penalty0.0001_ADMM_10.0_iter100.csv"^
     --type=SUR
-python energy.py --n=2 --num_edges=1 --evo_time=2 --n_ts=40^
+#python energy.py --n=2 --num_edges=1 --evo_time=2 --n_ts=40^
     --initial_control="../control/ADMM/EnergyADMM2_evotime2.0_n_ts40_ptypeWARM_offset0.5_penalty0.0001_ADMM_10.0_iter100.csv"^
     --type=minup --min_up=10
-python energy.py --n=2 --num_edges=1 --evo_time=2 --n_ts=40^
+#python energy.py --n=2 --num_edges=1 --evo_time=2 --n_ts=40^
     --initial_control="../control/ADMM/EnergyADMM2_evotime2.0_n_ts40_ptypeWARM_offset0.5_penalty0.01_ADMM_10.0_iter100.csv"^
     --type=maxswitch --max_switch=5
 cd ../../scripts/windows

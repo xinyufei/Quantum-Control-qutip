52c65af19322ff8501b9e95ecb87c3336b83ebb6
Xinyu <xyfei27@gmail.com>
2021-09-13 22:54:50 -0400

    organize the results

--- scripts/linux/energy_n6_nts40.sh
+++ scripts/linux/energy_n6_nts40.sh
@@ -13,9 +13,9 @@ cd ../ADMM/
 #python energy.py --n=6 --num_edges=3 --evo_time=2 --n_ts=40 --initial_type=WARM \
 #    --initial_control="../control/ADMM/EnergyADMM6_evotime2.0_n_ts40_ptypeWARM_offset0.5_penalty1e-05_ADMM_10.0_iter100.csv" \
 #    --alpha=1e-3 --rho=10 --max_iter_admm=100
-#python energy.py --n=6 --num_edges=3 --evo_time=2 --n_ts=40 --initial_type=WARM \
-#    --initial_control="../control/ADMM/EnergyADMM6_evotime2.0_n_ts40_ptypeWARM_offset0.5_penalty0.001_ADMM_10.0_iter100.csv" \
-#    --alpha=1e-2 --rho=10 --max_iter_admm=100
+python energy.py --n=6 --num_edges=3 --evo_time=2 --n_ts=40 --initial_type=WARM \
+    --initial_control="../control/ADMM/EnergyADMM6_evotime2.0_n_ts40_ptypeWARM_offset0.5_penalty0.001_ADMM_10.0_iter100.csv" \
+    --alpha=1e-2 --rho=15 --max_iter_admm=100
 #python energy.py --n=6 --num_edges=3 --evo_time=2 --n_ts=40 --initial_type=WARM \
 #    --initial_control="../control/ADMM/EnergyADMM6_evotime2.0_n_ts40_ptypeWARM_offset0.5_penalty0.01_ADMM_10.0_iter100.csv" \
 #    --alpha=1e-1 --rho=10 --max_iter_admm=100
@@ -86,12 +86,12 @@ cd ../Trustregion/
 #python energy.py --n=6 --num_edges=3 --evo_time=2 --n_ts=40 \
 #    --initial_file="../control/Rounding/Energy6_evotime2.0_n_ts40_ptypeCONSTANT_offset0.5_minup10.csv" \
 #    --alpha=0.01 --tr_type="hard" --hard_type="minup" --min_up=10
-python energy.py --n=6 --num_edges=3 --evo_time=2 --n_ts=40 \
-    --initial_file="../control/Rounding/EnergyADMM6_evotime2.0_n_ts40_ptypeWARM_offset0.5_penalty0.01_ADMM_10.0_iter100_maxswitch5.csv" \
-    --alpha=0.01 --tr_type="hard" --hard_type="maxswitch" --max_switch=5
-python energy.py --n=6 --num_edges=3 --evo_time=2 --n_ts=40 \
-    --initial_file="../control/Rounding/Energy6_evotime2.0_n_ts40_ptypeCONSTANT_offset0.5_maxswitch5.csv" \
-    --alpha=0.01 --tr_type="hard" --hard_type="maxswitch" --max_switch=5
+#python energy.py --n=6 --num_edges=3 --evo_time=2 --n_ts=40 \
+#    --initial_file="../control/Rounding/EnergyADMM6_evotime2.0_n_ts40_ptypeWARM_offset0.5_penalty0.01_ADMM_10.0_iter100_maxswitch5.csv" \
+#    --alpha=0.01 --tr_type="hard" --hard_type="maxswitch" --max_switch=5
+#python energy.py --n=6 --num_edges=3 --evo_time=2 --n_ts=40 \
+#    --initial_file="../control/Rounding/Energy6_evotime2.0_n_ts40_ptypeCONSTANT_offset0.5_maxswitch5.csv" \
+#    --alpha=0.01 --tr_type="hard" --hard_type="maxswitch" --max_switch=5
 
 #python energy.py --n=6 --num_edges=3 --evo_time=2 --n_ts=40 \
 #    --initial_file="../control/SwitchTime/EnergyST6_evotime_2.0_n_ts40_n_switch3_initwarm_minuptime0.0.csv" \
@@ -103,15 +103,15 @@ python energy.py --n=6 --num_edges=3 --evo_time=2 --n_ts=40 \
 #    --initial_file="../control/SwitchTime/EnergyST6_evotime_2.0_n_ts40_n_switch3_initwarm_minuptime0.0.csv" \
 #    --alpha=0.01 --tr_type="hard" --hard_type="maxswitch" --max_switch=5
 
-python energy.py --n=6 --num_edges=3 --evo_time=2 --n_ts=40 \
-    --initial_file="../control/Rounding/Energy6_evotime2.0_n_ts40_ptypeCONSTANT_offset0.5_alpha0.01_sigma0.25_eta0.001_threshold30_iter100_typetvc_SUR.csv" \
-    --alpha=0.01 --tr_type="tv"
-python energy.py --n=6 --num_edges=3 --evo_time=2 --n_ts=40 \
-    --initial_file="../control/Rounding/Energy6_evotime2.0_n_ts40_ptypeCONSTANT_offset0.5_alpha0.01_sigma0.25_eta0.001_threshold30_iter100_typetvc_minup10.csv" \
-    --alpha=0.01 --tr_type="hard" --hard_type="minup" --min_up=10
-python energy.py --n=6 --num_edges=3 --evo_time=2 --n_ts=40 \
-    --initial_file="../control/Rounding/Energy6_evotime2.0_n_ts40_ptypeCONSTANT_offset0.5_alpha0.01_sigma0.25_eta0.001_threshold30_iter100_typetvc_maxswitch5.csv" \
-    --alpha=0.01 --tr_type="hard" --hard_type="maxswitch" --max_switch=5
+#python energy.py --n=6 --num_edges=3 --evo_time=2 --n_ts=40 \
+#    --initial_file="../control/Rounding/Energy6_evotime2.0_n_ts40_ptypeCONSTANT_offset0.5_alpha0.01_sigma0.25_eta0.001_threshold30_iter100_typetvc_SUR.csv" \
+#    --alpha=0.01 --tr_type="tv"
+#python energy.py --n=6 --num_edges=3 --evo_time=2 --n_ts=40 \
+#    --initial_file="../control/Rounding/Energy6_evotime2.0_n_ts40_ptypeCONSTANT_offset0.5_alpha0.01_sigma0.25_eta0.001_threshold30_iter100_typetvc_minup10.csv" \
+#    --alpha=0.01 --tr_type="hard" --hard_type="minup" --min_up=10
+#python energy.py --n=6 --num_edges=3 --evo_time=2 --n_ts=40 \
+#    --initial_file="../control/Rounding/Energy6_evotime2.0_n_ts40_ptypeCONSTANT_offset0.5_alpha0.01_sigma0.25_eta0.001_threshold30_iter100_typetvc_maxswitch5.csv" \
+#    --alpha=0.01 --tr_type="hard" --hard_type="maxswitch" --max_switch=5
 
 #python energy.py --n=6 --num_edges=3 --evo_time=2 --n_ts=40 \
 #    --initial_file="../control/Continuous/Energy6_evotime2.0_n_ts40_ptypeCONSTANT_offset0.5.csv" \
